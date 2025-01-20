e28659bdbaef6c6ddfa3d83768d47827c0fb69b8
Asael <makintosh_ragga@yahoo.es>
2025-01-20 17:32:40 -0500

    feat: go to pokedex if already loggedIn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
 "use client";
+import { useLocalStorage } from "hooks/useLocalStorage";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 import { Login } from "screens/login/login";
 
 export default function Home() {
+  const [isLoggedIn] = useLocalStorage("isLoggedIn", false);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      router.push("/pokedex");
+    }
+  }, [isLoggedIn, router]);
+
   return (
     <main className="main-container">
       <Login />
