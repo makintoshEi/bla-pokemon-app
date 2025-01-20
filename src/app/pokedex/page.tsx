e2434dc9c9b8f59de479423171658598622fee9a
Asael <makintosh_ragga@yahoo.es>
2025-01-19 22:26:25 -0500

    refactor: change redirect (is used in ssr ) for useRouter from next/navigation

--- src/app/pokedex/page.tsx
+++ src/app/pokedex/page.tsx
@@ -3,19 +3,20 @@ import { useLocalStorage } from "hooks/useLocalStorage";
 import { useEffect } from "react";
 import { Pokedex } from "components/pokedex/pokedex";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { redirect } from "next/navigation";
 import { PokemonProvider } from "context/pokemon-context";
+import { useRouter } from "next/navigation";
 
 const queryClient = new QueryClient();
 
 export default function MainPokedex() {
   const [isLoggedIn] = useLocalStorage("isLoggedIn", false);
+  const router = useRouter();
 
   useEffect(() => {
     if (!isLoggedIn) {
-      redirect("/");
+      router.push("/");
     }
-  }, [isLoggedIn]);
+  }, [isLoggedIn, router]);
 
   return (
     <QueryClientProvider client={queryClient}>
