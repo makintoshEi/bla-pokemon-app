ced166192a904d14d457b9f40fc184e6fdb88aa9
Asael <makintosh_ragga@yahoo.es>
2025-01-20 16:04:13 -0500

    test: should search for bulbasaur

--- src/__tests__/pokedex.test.tsx
+++ src/__tests__/pokedex.test.tsx
@@ -1,4 +1,4 @@
-import { render, waitFor } from "@testing-library/react";
+import { fireEvent, render, waitFor } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Pokedex } from "screens/pokedex/pokedex";
 import { PokemonProvider } from "context/pokemon-context";
@@ -7,6 +7,7 @@ import "@testing-library/jest-dom";
 import { PokemonType } from "interfaces/pokemon";
 
 jest.mock("../api/pokemon.api");
+
 // Mock the debounce function
 jest.mock("lodash", () => ({
   debounce: jest.fn(() => console.log("debouncing")),
@@ -14,6 +15,87 @@ jest.mock("lodash", () => ({
 
 const mockGetPokemons = getPokemons as jest.MockedFunction<typeof getPokemons>;
 const mockGetPokemon = getPokemon as jest.MockedFunction<typeof getPokemon>;
+
+const mockPokemons = [
+  {
+    name: "bulbasaur",
+    url: "https://pokeapi.co/api/v2/pokemon/1/",
+  },
+  {
+    name: "ivysaur",
+    url: "https://pokeapi.co/api/v2/pokemon/2/",
+  },
+  {
+    name: "venusaur",
+    url: "https://pokeapi.co/api/v2/pokemon/3/",
+  },
+  {
+    name: "charmander",
+    url: "https://pokeapi.co/api/v2/pokemon/4/",
+  },
+];
+
+const mockPokemon = {
+  abilities: [
+    {
+      ability: {
+        name: "overgrow",
+        url: "https://pokeapi.co/api/v2/ability/65/",
+      },
+    },
+    {
+      ability: {
+        name: "chlorophyll",
+        url: "https://pokeapi.co/api/v2/ability/34/",
+      },
+    },
+  ],
+  forms: [
+    {
+      name: "bulbasaur",
+      url: "https://pokeapi.co/api/v2/pokemon-form/1/",
+    },
+  ],
+  id: 1,
+  moves: [
+    {
+      move: {
+        name: "razor-wind",
+      },
+    },
+    {
+      move: {
+        name: "swords-dance",
+      },
+    },
+    {
+      move: {
+        name: "cut",
+      },
+    },
+  ],
+  sprites: {
+    other: {
+      home: {
+        front_default:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png",
+      },
+      "official-artwork": {
+        front_default:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      },
+    },
+  },
+  types: [
+    {
+      slot: 1,
+      type: {
+        name: "grass" as PokemonType,
+        url: "https://pokeapi.co/api/v2/type/12/",
+      },
+    },
+  ],
+};
 
 describe("<Pokedex />", () => {
   let queryClient: QueryClient;
@@ -27,7 +109,6 @@ describe("<Pokedex />", () => {
       },
     });
 
-    // Reset all mocks before each test
     jest.resetAllMocks();
   });
 
@@ -53,87 +134,6 @@ describe("<Pokedex />", () => {
   });
 
   it("should render pokemon list after loading", async () => {
-    const mockPokemons = [
-      {
-        name: "bulbasaur",
-        url: "https://pokeapi.co/api/v2/pokemon/1/",
-      },
-      {
-        name: "ivysaur",
-        url: "https://pokeapi.co/api/v2/pokemon/2/",
-      },
-      {
-        name: "venusaur",
-        url: "https://pokeapi.co/api/v2/pokemon/3/",
-      },
-      {
-        name: "charmander",
-        url: "https://pokeapi.co/api/v2/pokemon/4/",
-      },
-    ];
-
-    const mockPokemon = {
-      abilities: [
-        {
-          ability: {
-            name: "overgrow",
-            url: "https://pokeapi.co/api/v2/ability/65/",
-          },
-        },
-        {
-          ability: {
-            name: "chlorophyll",
-            url: "https://pokeapi.co/api/v2/ability/34/",
-          },
-        },
-      ],
-      forms: [
-        {
-          name: "bulbasaur",
-          url: "https://pokeapi.co/api/v2/pokemon-form/1/",
-        },
-      ],
-      id: 1,
-      moves: [
-        {
-          move: {
-            name: "razor-wind",
-          },
-        },
-        {
-          move: {
-            name: "swords-dance",
-          },
-        },
-        {
-          move: {
-            name: "cut",
-          },
-        },
-      ],
-      sprites: {
-        other: {
-          home: {
-            front_default:
-              "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png",
-          },
-          "official-artwork": {
-            front_default:
-              "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-          },
-        },
-      },
-      types: [
-        {
-          slot: 1,
-          type: {
-            name: "grass" as PokemonType,
-            url: "https://pokeapi.co/api/v2/type/12/",
-          },
-        },
-      ],
-    };
-
     mockGetPokemons.mockResolvedValue({
       count: 1400,
       next: "",
@@ -160,6 +160,27 @@ describe("<Pokedex />", () => {
       expect(
         getByText("Error loading pokemons, refresh the page")
       ).toBeInTheDocument();
+    });
+  });
+
+  it("should search for bulbasaur", async () => {
+    mockGetPokemons.mockResolvedValue({
+      count: 1400,
+      next: "",
+      previous: "",
+      results: mockPokemons,
+    });
+
+    mockGetPokemon.mockResolvedValue(mockPokemon);
+
+    const { getByText, getByPlaceholderText } = renderWithQueryClient();
+    await waitFor(() => {
+      const searchBarInput = getByPlaceholderText("Search pokemon");
+      fireEvent.change(searchBarInput, {
+        target: { value: "bulbasaur" },
+      });
+      expect(searchBarInput).toHaveValue("bulbasaur");
+      expect(getByText("Bulbasaur")).toBeInTheDocument();
     });
   });
 });
