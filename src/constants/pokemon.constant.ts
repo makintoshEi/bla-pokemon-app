6cf287ec3b6c94552b082930ec3c54bfc006e0fd
Asael <makintosh_ragga@yahoo.es>
2025-02-12 11:32:04 -0500

    refactor: handle optional chaining for pagination limits

--- src/constants/pokemon.constant.ts
+++ src/constants/pokemon.constant.ts
@@ -20,7 +20,7 @@ const POKEMON_TYPE: Record<PokemonType, string> = {
     'steel': '#B7B9D0',
     'water': '#6493EB',
 }
-const LIMITS = process.env.NEXT_PUBLIC_PAGINATION_LIMIT!.split(",").map((limit) => +limit) || [25, 50, 100];
+const LIMITS = process.env.NEXT_PUBLIC_PAGINATION_LIMIT?.split(",").map((limit) => +limit) || [25, 50, 100];
 const PAGINATION_LIMIT = LIMITS[0];
 const DEBOUNCE_TIME = +(process.env.NEXT_PUBLIC_DEBOUNCE_TIME || 300);
 const STALE_TIME = +(process.env.NEXT_PUBLIC_STALE_TIME_IN_MIN || 60) * 60 * 1000;
